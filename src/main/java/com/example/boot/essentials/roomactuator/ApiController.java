182da4dc9242ff09bf636c54365de355786a5233
Navid Khalili <nkhalili@live.com>
2020-08-04 17:57:38 -0400

    Extend Metrics by adding counter

--- src/main/java/com/example/boot/essentials/roomactuator/ApiController.java
+++ src/main/java/com/example/boot/essentials/roomactuator/ApiController.java
@@ -1,5 +1,8 @@
 package com.example.boot.essentials.roomactuator;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +15,23 @@ import java.util.List;
 @RequestMapping("api")
 public class ApiController {
     private final PresidentRepository presidentRepository;
+    private final Counter greetingCounter;
 
     @Autowired
-    public ApiController(PresidentRepository presidentRepository) {
+    public ApiController(PresidentRepository presidentRepository, MeterRegistry registry) {
+
         this.presidentRepository = presidentRepository;
+        this.greetingCounter = Counter.builder("api.greeting").register(registry);
     }
 
     @GetMapping("greeting")
-    public String getGreeting() {return "Hello there!";}
+    public String getGreeting() {
+        greetingCounter.increment();
+        return "Hello there!";
+    }
 
     @GetMapping("presidents")
+    @Timed(value="api.getAllPresidents")
     public List<President> getAllPresidents(){
         List<President> presidents = new ArrayList<>();
         this.presidentRepository.findAll().forEach(presidents::add);
