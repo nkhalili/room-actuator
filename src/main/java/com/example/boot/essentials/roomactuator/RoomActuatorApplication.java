797473886572b6deb25512cb9678c3a9605283c3
Navid Khalili <nkhalili@live.com>
2020-08-04 14:57:43 -0400

    Init

---
+++ src/main/java/com/example/boot/essentials/roomactuator/RoomActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot.essentials.roomactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boot/essentials/roomactuator/RoomActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot.essentials.roomactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
